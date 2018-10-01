0a01491c9b36301114cb91d0b83a705fba9a3a88
Guillaume Gérard <guillaume.gerard88@gmail.com>
2018-10-01 16:40:50 +0200

    Update Ember configuration to fit Ember 3.4 base requirement

--- ember.js
+++ ember.js
@@ -7,10 +7,32 @@ module.exports = {
     server: true, // mirage
     withFeature: true // feature flag
   },
+  plugins: ['ember'],
   extends: [
     'eslint:recommended',
     'plugin:ember/recommended',
     'plugin:ember-suave/recommended',
     './core.js'
+  ],
+  overrides: [
+    // node files
+    {
+      files: [
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    }
   ]
 };
